5ccfa339ded4f05b790585a51523d5bccc2ef245
sumin <suminc255@gmail.com>
2024-04-14 23:13:08 +0900

    no message

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -1,7 +1,49 @@
-import React from "react";
+// 30
+import React, { useState } from "react";
 
 const Add = () => {
-  return <div></div>;
+  const [book, setBook] = useState({
+    title: "",
+    desc: "",
+    price: null,
+    cover: "",
+  });
+
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  console.log(book);
+
+  return (
+    <div className="form">
+      <h1>Add New Book</h1>
+      <input
+        type="text"
+        placeholder="title"
+        onChange={handleChange}
+        name="title"
+      />
+      <input
+        type="text"
+        placeholder="desc"
+        onChange={handleChange}
+        name="desc"
+      />
+      <input
+        type="number"
+        placeholder="price"
+        onChange={handleChange}
+        name="price"
+      />
+      <input
+        type="text"
+        placeholder="cover"
+        onChange={handleChange}
+        name="cover"
+      />
+    </div>
+  );
 };
 
 export default Add;
--- client/src/pages/Books.jsx
+++ client/src/pages/Books.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 
 const Books = () => {
@@ -7,15 +8,33 @@ const Books = () => {
   useEffect(() => {
     const fetchAllBooks = async () => {
       try {
-        const res = await axios.get("http:localhost:8800/books");
-        console.log(res);
+        const res = await axios.get("http://localhost:8800/books");
+        setBooks(res.data);
       } catch (err) {
         console.log(err);
       }
     };
+    fetchAllBooks();
   }, []);
 
-  return <div>Books</div>;
+  return (
+    <div>
+      <h1>Book Shop</h1>
+      <div className="books">
+        {books.map((book) => (
+          <div className="book" key={book.id}>
+            {book.cover && <img src={book.cover} alt="" />}
+            <h2>{book.title}</h2>
+            <p>{book.desc}</p>
+            <span>{book.price}</span>
+          </div>
+        ))}
+      </div>
+      <button>
+        <Link to="/add">Add new book</Link>
+      </button>
+    </div>
+  );
 };
 
 export default Books;
